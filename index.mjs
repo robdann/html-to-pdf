374a4f896ef6a89a84f515548230ba7c2d3cd5a8
robertdann <robdann@gmail.com>
2025-05-19 17:45:20 +0100

    First attempt at PDF printer

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import express from 'express';
+import {chromium} from "playwright-core";
+
+const app = express();
+
+app.use(express.json());
+app.post('/', async (req, res) => {
+    const {url} = req.body;
+    const browser = await chromium.launch({
+        headless: true
+    });
+    try {
+        const page = await browser.newPage();
+        await page.goto(url);
+        const buffer = await page.pdf({displayHeaderFooter: false});
+        res.set('Content-Type', 'application/pdf');
+        res.send(buffer);
+    } catch (e) {
+        res.status(500).send({error: e});
+    } finally {
+        await browser.close();
+    }
+})
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("html-to-pdf listening for inputs!");
+});
