b9be12d3e29674b4c3b2cd66e93ec762e2087230
robertdann <robdann@gmail.com>
2025-09-16 09:30:15 +0100

    Accept the html instead of a url

--- index.mjs
+++ index.mjs
@@ -5,44 +5,47 @@ import morgan from "morgan";
 const app = express();
 app.use(morgan('combined'));
 app.use(express.json());
+app.use(express.text());
+app.use(express.raw());
+
 app.post('/', async (req, res) => {
-    const {url} = req.body;
+    const text = req.body;
 
-    const html = await fetch(url);
-    if (!html.ok) {
-        console.warn(`Could not find html from ${url}`);
-        res.status(400);
-        return;
-    }
-
-    const browser = await chromium.launch({
-        headless: true
+    const tempApp = express();
+    tempApp.get(`/doc.html`, (req, res) => {
+        res.status(200).send(text);
     });
-    try {
-        const page = await browser.newPage();
-        console.log("html-to-pdf", url);
-        await page.goto(url);
-        console.log("html-to-pdf", 'waitForLoadState', 'networkidle');
-        await page.waitForLoadState('networkidle', {timeout: 10000});
-        console.log("html-to-pdf", 'pdf');
-        const buffer = await page.pdf({displayHeaderFooter: false});
-        res.set('Content-Type', 'application/pdf');
-        res.set('Content-Length', `${buffer.length}`);
-        console.log("html-to-pdf", 'send');
-        res.send(buffer);
-    } catch (e) {
-        console.log(e);
-        console.log(e.stack);
-        res.status(500).send({error: e.message});
-    } finally {
-        await browser.close();
-    }
+    await new Promise((resolve, reject) => {
+        const tempServer = tempApp.listen(0, async () => {
+            const url = `http://localhost:${tempServer.address().port}/doc.html`;
+            const browser = await chromium.launch({
+                headless: true
+            });
+            try {
+                const page = await browser.newPage();
+                console.log("html-to-pdf", url);
+                await page.goto(url);
+                console.log("html-to-pdf", 'waitForLoadState', 'networkidle');
+                await page.waitForLoadState('networkidle', {timeout: 10000});
+                console.log("html-to-pdf", 'pdf');
+                const buffer = await page.pdf({displayHeaderFooter: false});
+                res.set('Content-Type', 'application/pdf');
+                res.set('Content-Length', `${buffer.length}`);
+                console.log("html-to-pdf", 'send');
+                res.send(buffer);
+            } catch (e) {
+                console.log(e);
+                console.log(e.stack);
+                res.status(500).send({error: e.message});
+            } finally {
+                await browser.close();
+                resolve();
+            }
+        })
+    });
 })
 
 app.listen(process.env.PORT || 8080, async () => {
     console.log("html-to-pdf listening for inputs!");
-
-    const response = await fetch('http://afyaca-server.railway.internal:9000');
-    console.log(response.statusText);
 
 });
